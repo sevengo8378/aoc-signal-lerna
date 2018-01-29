bcf3bf5fe189478a0771a43a663ac322bc398b01
byrne.liang <byrne.liang@alo7.com>
2018-01-29 16:40:24 +0800

    feat: 增加国内作为消息发送方的测试; 测试断线间隔时间调整为30分钟; 日志采用apend模式

--- ansible/two_machine_benchmark.sh
+++ ansible/two_machine_benchmark.sh
@@ -9,13 +9,19 @@ action=$5
 cron_action=remove
 [[ "$action" == "start" ]] && cron_action=add
 
-duration=60
+duration=1800
 
 # 国内机器消息接收
 ansible-playbook benchmark.yml -l ${cn_device} --tags cron_recv_${cron_action} --extra-vars "room=${room} zx=false"
 
 # 海外机器消息发送
 ansible-playbook benchmark.yml -l ${oversea_device} --tags cron_send_${cron_action} --extra-vars "room=${room} zx=false"
+
+# 海外机器消息接收
+ansible-playbook benchmark.yml -l ${oversea_device} --tags cron_recv_${cron_action} --extra-vars "room=${room}_reverse zx=false"
+
+# 国内机器消息发送
+ansible-playbook benchmark.yml -l ${cn_device} --tags cron_send_${cron_action} --extra-vars "room=${room}_reverse zx=false"
 
 # 国内机器测试断线率
 ansible-playbook benchmark.yml -l ${cn_device} --tags cron_recv_single_${cron_action} --extra-vars "room=${room}_single duration=${duration} zx=false"
@@ -28,6 +34,12 @@ ansible-playbook benchmark.yml -l ${oversea_device} --tags cron_recv_single_${cr
 
 # 海外机器消息发送(专线模式)
 [[ "$use_zx" == "true" ]] && ansible-playbook benchmark.yml -l ${oversea_device} --tags cron_send_${cron_action} --extra-vars "room=${room}_zx zx=true"
+
+# 海外机器消息接收(专线模式)
+[[ "$use_zx" == "true" ]] && ansible-playbook benchmark.yml -l ${oversea_device} --tags cron_recv_${cron_action} --extra-vars "room=${room}_zx_reverse zx=true"
+
+# 国内机器消息发送(专线模式)
+[[ "$use_zx" == "true" ]] && ansible-playbook benchmark.yml -l ${cn_device} --tags cron_send_${cron_action} --extra-vars "room=${room}_zx_reverse zx=false"
 
 # 海外机器测试断线率(专线模式)
 [[ "$use_zx" == "true" ]] && ansible-playbook benchmark.yml -l ${oversea_device} --tags cron_recv_single_${cron_action} --extra-vars "room=${room}_single duration=${duration} zx=true"
